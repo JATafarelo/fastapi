14422251168f0a9e1fb34ad7aa3f6c686c388018
JATafarelo <jguelfi@analytics2go.com>
2022-01-14 09:58:00 -0300

    function to update user

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from uuid import UUID
 from fastapi import FastAPI,HTTPException
 from typing import List
-from models import User,Gender,Role
+from models import User,Gender,Role, UserUpdateRequest
 
 #python -m uvicorn main:app --reload
 
@@ -39,6 +39,24 @@ async def delete_user(user_id:UUID):
     for user in db:
         if user.id == user_id:
             db.remove(user)
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exists"
+    )
+
+@app.put("/api/v1/user/{user_id}")
+async def update_user(user_update : UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user_update.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user_update.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user_update.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user_update.roles = user_update.roles
             return
     raise HTTPException(
         status_code=404,
